75fe559c380e9f176097f2468253ef58ed7affc7
ormware <69240937+ormware@users.noreply.github.com>
2023-04-10 10:05:57 +0200

    Update wardrive.sh

--- scripts/wardrive.sh
+++ scripts/wardrive.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-pulseaudio --start
-paplay wav/wardriveArmed.wav
-sudo mv /home/pi/*.netxml /home/pi/netxml/
-sudo rm /home/pi/Kismet*
+#commented audio cosmetics for debugging reasons
+#pulseaudio --start
+#paplay wav/wardriveArmed.wav
+
+mkdir -p ~/scripts/netxml
+sudo mv ~/*.netxml ~/scripts/netxml/
+sudo rm ~/Kismet*
 sudo systemctl stop gpsd.socket; sleep 1
 sudo systemctl disable gpsd.socket; sleep 1
 sudo gpsd /dev/ttyACM0 -F /var/run/gpsd.sock
+if [ $? -eq 1 ]
+then
+  sudo apt install -y gpsd
+  echo "Dependencies successfully installed. Kindly relaunch."; sleep 3
+  exit 0
+fi
+
 sudo airmon-ng check kill; sleep 1
 sudo airmon-ng start wlan1; sleep 1
-lxterminal -e "bash kismet -c wlan1mon"
+tmux split-window -v "bash kismet -c wlan1mon"
